5ddbb47e7ff1671c5d1edc5085dc08610979791e
Rinat Khabibiev <srenskiy@gmail.com>
2017-07-14 12:44:15 +0300

    added practical examples and home work

---
+++ binary_search.py
@@ -0,0 +1,24 @@
+ordered_elements = [1, 2, 3, 12, 15, 18, 19, 21, 22, 33, 43, 44, 45]
+
+
+def search(element, elements):
+    for index, candidate in enumerate(elements):
+        if candidate == element:
+            return index
+    return -1
+
+
+def binary_search(element, elements, delta=0):
+    length = len(elements)
+    if length <= 2:
+        result = search(element, elements)
+        return result if result == -1 else result + delta
+    index = length // 2
+    candidate = elements[index]
+    if candidate > element:
+        return binary_search(element, elements[:index], delta)
+    else:
+        return binary_search(element, elements[index:], delta + index)
+
+
+print(binary_search(21, ordered_elements))
---
+++ unique.py
@@ -0,0 +1,15 @@
+unordered_elements = [12, 43, 1, 3, 1, 3, 44, 21, 2, 18, 15, 19, 1, 33, 45, 2, 22, 2]
+
+
+def unique(elements):
+    seen = set()
+    for element in elements:
+        if element in seen:
+            continue
+        seen.add(element)
+        yield element
+
+
+print(list(unique(unordered_elements)))
+
+print(list(set(unordered_elements)))
